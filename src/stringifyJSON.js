43cc150ebe129e6198cd4beec21455ef19f3f931
MichaelGofron <gofronm@yahoo.com>
2015-06-12 20:51:46 -0500

    Completed array and before parts of stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,20 @@
 
 // but you don't so you're going to write it from scratch:
 
+// Must use recursion on objects and arrays
+// Then implement algorithm on the primitives
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (Array.isArray(obj)){
+    // perform on each element stringifyJSON
+    obj = _.map(obj,stringifyJSON);
+    return "[" + obj + "]";
+  }else if (typeof obj === 'object' && obj !== null){
+    console.log("object");
+  }else{ // primitive type, replace '' with "" then stringify
+    if (typeof obj === "string"){ // string type
+      return '\"' + obj + '\"'
+    }
+    return String(obj);
+  }
 };
